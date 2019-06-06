962c6391dbc4d265429985612afdb15d2c421ea3
snowleo208 <13027432+snowleo208@users.noreply.github.com>
2019-06-06 09:48:40 +0800

    fix: prod setting in env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBpL62sKC2d7za0U6CZVIs3nyXJ9BrD3I4',
+    authDomain: 'my-first-project-bdc4f.firebaseapp.com',
+    databaseURL: 'https://my-first-project-bdc4f.firebaseio.com',
+    projectId: 'my-first-project-bdc4f',
+    storageBucket: 'my-first-project-bdc4f.appspot.com',
+    messagingSenderId: '77995711303'
+  }
 };
